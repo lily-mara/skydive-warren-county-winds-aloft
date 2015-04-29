28c188ccc8adb4a541ed4de9049fde68ba75d4ca
Nate Mara <natemara@gmail.com>
2015-04-29 19:08:14 -0400

    add ground wind from metars

--- app.py
+++ app.py
@@ -4,10 +4,33 @@ import tornado.ioloop
 import tornado.web
 import tornado.autoreload
 from tornado.options import define, options, parse_command_line
+from metar.Metar import Metar
+import requests
 
 from aloft import winds_aloft
 
 BASE_PATH = os.path.dirname(os.path.realpath(__file__))
+BASE_ADDR = 'http://weather.noaa.gov/pub/data/observations/metar/stations/{}.TXT'
+
+
+def add_ground_wind(airport_code, aloft_wind):
+	raw = requests.get(BASE_ADDR.format(airport_code.upper()))
+	if raw.status_code == 404:
+		raw = requests.get(BASE_ADDR.format('K' + airport_code.upper()))
+
+	if raw.status_code == 404:
+		raise ValueError('The given station code is invalid.')
+
+	ground_wind = {'altitude': 0, 'direction': 0, 'speed': 0}
+
+	for line in raw.text.split('\n'):
+		if airport_code.upper() in line:
+			line = line.strip()
+			observation = Metar(line)
+			ground_wind['direction'] = observation.wind_dir.value()
+			ground_wind['speed'] = observation.wind_speed.value(units='KT')
+
+	return [ground_wind] + aloft_wind
 
 
 class IndexHandler(tornado.web.RequestHandler):
@@ -17,7 +40,10 @@ class IndexHandler(tornado.web.RequestHandler):
 
 class WindsAloftHandler(tornado.web.RequestHandler):
 	def get(self, airport_code):
-		self.finish(winds_aloft(airport_code).dict())
+		aloft_info = winds_aloft(airport_code).dict()
+		aloft_info['winds'] = add_ground_wind(airport_code, aloft_info['winds'])
+
+		self.finish(aloft_info)
 
 handlers = [
 	(r'/', IndexHandler),
